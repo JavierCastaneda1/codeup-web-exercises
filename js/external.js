921271ef3d93a9d3913746327e692c448d9b9d66
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-06 13:28:40 -0500

    completed exercise

--- js/external.js
+++ js/external.js
@@ -6,7 +6,54 @@
 
 //alert("Cool my favorite color is "+userInput+" too");
 
-var userInput = prompt('How many days will you rent this movie?');
+
+
+// the following is question # 3 part 1
+//var userInput = prompt('How many days would you like to rent The Little Mermaid?');
 console.log('The user entered: ' + userInput);
+
+//var userInput = prompt('How many days would you like to rent Brother Bear?');
+console.log('The user entered: ' + userInput);
+
+//var userInput = prompt('How many days would you like to rent Hercules');
+console.log('The user entered: ' + userInput);
+
+//var total = (userInput * 3)
+
+//alert("This transaction will be "+total+" dollars");
+
+
+//the following is question #3 part 2
+
+var userInput = prompt('How many hours did you work for Google?');
+console.log('The user entered: ' + userInput);
+
+var userInput = prompt('How many hours did you work for Amazon?');
+console.log('The user entered: ' + userInput);
+
+var userInput = prompt('How many hours did you work for Facebook?');
+console.log('The user entered: ' + userInput);
+
+var paygoogle = 400
+
+var pay2g = (paygoogle * userInput)
+
+var payamazon = 380
+
+var pay2a = (payamazon * userInput)
+
+var payfacebook = 350
+
+var pay2f = (payfacebook * userInput)
+
+var result = pay2g + pay2a + pay2f
+
+alert("This transaction will be "+result+" dollars");
+
+
+
+
+
+
 
 
