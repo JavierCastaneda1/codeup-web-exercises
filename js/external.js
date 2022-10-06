583fa373b492310eb23edea7a86efec8e0665118
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-06 11:08:32 -0500

    working on exercise

---
+++ js/external.js
@@ -0,0 +1,12 @@
+
+//console.log('Hello from external JavaScript!');
+
+//var userInput = prompt('What is your favorite color?');
+//console.log('The user entered: ' + userInput);
+
+//alert("Cool my favorite color is "+userInput+" too");
+
+var userInput = prompt('How many days will you rent this movie?');
+console.log('The user entered: ' + userInput);
+
+
