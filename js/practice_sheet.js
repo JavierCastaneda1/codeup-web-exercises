a1fd10196a026eeaf2e172668d9abf73f6adf466
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-11 15:36:26 -0500

    working on conditionals exercise

---
+++ js/practice_sheet.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//*example of console.log taking "user input" from prompt.*
+//var userInput = prompt('Please type something:');
+//console.log('The user entered: ' + userInput);
+
+//*example of a "box pop up" type alert on a website. Note: has a "ok" button.*
+//alert("Welcome to my website!");
+
+var userInput = prompt('What is your favorite color?');
+console.log('The user entered: ' + userInput);
+
+
