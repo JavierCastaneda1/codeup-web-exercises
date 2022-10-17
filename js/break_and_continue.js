01d6c6d5ab11dd5bc550ce4fd1e2220fcdb4eb27
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-17 09:28:14 -0500

    editing break and continue with walkthrough notes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,38 @@
-function skipLoop() {
-    let input;
-    while (true) {
-        input = parseFloat(prompt('input a number 1-50, but is odd'))
-        if (input % 2 !== 0 && input >= 1 && input <= 50) {
-            break;
-        }
+
+//codys example
+
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+
+// do{
+//     let userInput = prompt("please enter and odd number between 1 and 50");
+//
+//     if(userInput % 2 !== 0){
+//         alert("Your input was even, please try again.");
+//     } else if (userInput < 1){
+//         alert("Your input is less than 1");
+//     } else if (userInput > 50){
+//         alert("Your input is greater than 50. Please try again.");
+//     } else {
+//         alert("Valid input");
+//         break;
+//     }
+//
+// } while(true)
+
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+let userInput = prompt("Please enter an odd number between 1 and 50");
+console.log("The number to skip is " +userInput);
+for(let i = 1; i<=50; i+= 2){
+    if(userInput == i){
+        console.log("Yikes skipping number "+userInput);
+        continue;
     }
-    for (let i = 1; i < 50; i++) {
-        if (input === i) {
-            console.log(`Whoops, look like were skipping: ${input}`)
-            continue;
-        } else if (i % 2 === 0) {
-            continue;
-        }
-        console.log(`here is an odd number: ${i}`);
-    }
+    console.log("Here is an odd number "+i);
 }
-skipLoop();
+
+
+
+
+
+
