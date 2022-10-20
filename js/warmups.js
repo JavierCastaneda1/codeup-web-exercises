0f1dc08231d8c8fed27fa5337901900dbfd19138
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-20 09:22:01 -0500

    warmups

---
+++ js/warmups.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//October 20th 2022
+
+// Write a function that when passed an object will return the value of the object’s price property.
+//     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+// getPrice(obj); // returns “$7.89”
+
+function getPrice(obj){
+    return obj.price;
+}
+
+let potatoes = {
+    name: "Potatoes",
+    quantity: 2,
+    price: "$7.89"
+};
+
+
+
+console.log(getPrice(potatoes))
+
+
+
+
+
+
+
+
