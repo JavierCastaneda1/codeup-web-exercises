79aec0e406ba713000cb5d1c321da8a98601430b
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-21 09:30:52 -0500

    completed warm up exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,10 +1,12 @@
 "use strict";
 
-//October 20th 2022
+////warm up 10/20/22************
 
 // Write a function that when passed an object will return the value of the object’s price property.
 //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // getPrice(obj); // returns “$7.89”
+
+
 
 function getPrice(obj){
     return obj.price;
@@ -16,12 +18,46 @@ let potatoes = {
     price: "$7.89"
 };
 
-
-
 console.log(getPrice(potatoes))
 
 
 
+
+//warm up 10/21/22*************
+// Write a function that when passed an array will return the longest string from the array.
+//     Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
+// getLongestString(arr) // Returns “Periwinkle”
+
+
+
+// function longestArray(arra)
+// {
+//     let maxStr = arra[0].length;
+//     let longestStr = arra[0];
+//     for (let i = 1; i < arra.length; i++) {
+//         let maxi = arra[i].length;
+//         if (maxi > maxStr) {
+//             longestStr = arra[i];
+//             maxStr = maxi;
+//         }
+//     }
+//     return longestStr;
+// }
+//
+// console.log(longestArray(["Green", "Blue", "Periwinkle"]));
+
+//David's Example
+function getLongest(arr){
+    let longestString = ""
+    for (let str of arr) {
+        if (str.length > longestString.length){
+            longestString = str;
+        }
+    }
+    return longestString;
+}
+const colors = ["Green", "Blue", "Periwinkle"];
+console.log(getLongest(colors));
 
 
 
