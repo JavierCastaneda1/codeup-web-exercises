d749bfb46d9424934b919f989f7aca9d62450332
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-26 09:58:13 -0500

    working on warmups

--- js/warmups.js
+++ js/warmups.js
@@ -1,32 +1,30 @@
 "use strict";
 
 ////warm up 10/20/22************
-
-// Write a function that when passed an object will return the value of the object’s price property.
+//TODO: Write a function that when passed an object will return the value of the object’s price property.
 //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // getPrice(obj); // returns “$7.89”
 
 
 
-function getPrice(obj){
-    return obj.price;
-}
-
-let potatoes = {
-    name: "Potatoes",
-    quantity: 2,
-    price: "$7.89"
-};
-
-console.log(getPrice(potatoes))
+// function getPrice(obj){
+//     return obj.price;
+// }
+//
+// let potatoes = {
+//     name: "Potatoes",
+//     quantity: 2,
+//     price: "$7.89"
+// };
+//
+// console.log(getPrice(potatoes))
 
 
 
 
 //warm up 10/21/22*************
-// Write a function that when passed an array will return the longest string from the array.
-//     Ex. const arr = [“Green”, “Blue”, “Periwinkle”];
-// getLongestString(arr) // Returns “Periwinkle”
+// TODO: Write a function that when passed an array will return the longest string from the array.
+//     Ex. const arr = [“Green”, “Blue”, “Periwinkle”]; getLongestString(arr) // Returns “Periwinkle”
 
 
 
@@ -47,19 +45,65 @@ console.log(getPrice(potatoes))
 // console.log(longestArray(["Green", "Blue", "Periwinkle"]));
 
 //David's Example
-function getLongest(arr){
-    let longestString = ""
-    for (let str of arr) {
-        if (str.length > longestString.length){
-            longestString = str;
-        }
+// function getLongest(arr){
+//     let longestString = ""
+//     for (let str of arr) {
+//         if (str.length > longestString.length){
+//             longestString = str;
+//         }
+//     }
+//     return longestString;
+// }
+// const colors = ["Green", "Blue", "Periwinkle"];
+// console.log(getLongest(colors));
+
+//10/26/22 warm up (how to find the tallest hamster and return entire object)
+// TODO: Write a function that takes in an array of objects and returns the object with the largest height property.
+
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "September 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
     }
-    return longestString;
+];
+
+getTallestHamster(hamsters); // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
+
+function getTallestHamster(arr){
+    let tallest = {heightInMM: 0} ;     //start with establishing a height
+    arr.forEach(function(el){
+        if (el.heightInMM > tallest.heightInMM){
+            tallest = el;
+        }
+    });
+    return tallest;
 }
-const colors = ["Green", "Blue", "Periwinkle"];
-console.log(getLongest(colors));
-
-
-
-
-
+console.log(getTallestHamster(hamsters));
