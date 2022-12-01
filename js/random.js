ff63fb55d31ea5f5e4d076943ac7456ec1620934
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-11-30 19:39:41 -0600

    practice

--- js/random.js
+++ js/random.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
-// HIDES PARAGRAPH
-    $(`p`).hide();
-// SELECTED DIV ON CLICK WILL TOGGLE
-    $(`#godDiv`).on(`click`, function() {
-        $(`#godPara`).toggle();
-    })
-    // $( "#submitButton" ).click(function() {
-    //   $(`#special`).delay(800).toggle();
-    // })
-    $("#submitButton").on("click",function() {
+
+    $(`.acePara`).hide();
+    $(`.special`).hide();
+
+    $(`#aceDiv`).on(`click`, function() {
+        $(`.acePara`).toggle();
+    });
+
+    $(".pinkButton").on("click",function() {
         setTimeout(function() {
             $('.container').css(`background-color`, 'pink');}, 3000);
     });
